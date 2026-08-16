9339c9619e8c72a4a638d5cdd78807eea1807bae
Lloyd Everett <lloydeverett@gmail.com>
2026-08-16 16:09:12 +0200

    change commands in entry to suggested commands

--- src/entry.rs
+++ src/entry.rs
@@ -8,8 +8,5 @@ pub struct Entry {
     pub id: Vec<String>,
     pub is_dir: bool,
     pub is_link: bool,
-    /// Commands available on this entry. Shared rather than owned per-entry
-    /// since many entries from the same source typically expose the same
-    /// set of commands.
-    pub commands: Arc<[Command]>,
+    pub suggested_commands: Arc<[Command]>,
 }
